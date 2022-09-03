45743bb8f3c6b49209575f46598b1eeeb8576842
Bhakti Ghaghda <72260135+Butkii@users.noreply.github.com>
2022-09-03 20:41:50 +0400

    Progress

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -15,12 +15,17 @@ class NotesService {
   DatabaseUser? _user;
 
   static final NotesService _shared = NotesService._sharedInstance();
-  NotesService._sharedInstance();
+  NotesService._sharedInstance() {
+    _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
+      onListen: () {
+        _notesStreamController.sink.add(_notes);
+      },
+    );
+  }
+
   factory NotesService() => _shared;
 
-  // late final StreamController<List<DatabaseNote>> _notesStreamController;
-  final _notesStreamController =
-      StreamController<List<DatabaseNote>>.broadcast();
+  late final StreamController<List<DatabaseNote>> _notesStreamController;
 
   Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
 
