fc3c7b7c633ae8e969cec6a1ed84f1ff6c69358f
Bhakti Ghaghda <72260135+Butkii@users.noreply.github.com>
2022-09-03 22:53:28 +0400

    Build delete notes feature

---
+++ lib/views/notes/notes_list_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/services/crud/notes_services.dart';
+import 'package:mynotes/utilities/dialogs/delete_dialog.dart';
+
+typedef DeleteNoteCallback = void Function(DatabaseNote note);
+
+class NotesListView extends StatelessWidget {
+  final List<DatabaseNote> notes;
+  final DeleteNoteCallback onDeleteNote;
+
+  const NotesListView({
+    Key? key,
+    required this.notes,
+    required this.onDeleteNote,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        final note = notes[index];
+        return ListTile(
+          title: Text(
+            note.text,
+            maxLines: 1,
+            softWrap: true,
+            overflow: TextOverflow.ellipsis,
+          ),
+          tileColor: Color.fromARGB(255, 228, 245, 254),
+          trailing: IconButton(
+            icon: const Icon(Icons.delete),
+            // color: Colors.blueGrey,
+            onPressed: () async {
+              final shouldDelete = await showDeleteDialog(context);
+              if (shouldDelete) {
+                onDeleteNote(note);
+              }
+            },
+          ),
+        );
+      },
+    );
+  }
+}
